eedea181426f098a02d8ba947285b697e59c2c53
Alina Akompi <alina.akompi@gmail.com>
2021-07-29 22:51:37 +0300

    add loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -5,7 +5,7 @@ import styles from "./Loader.module.css";
 const Loader = () => {
   return (
     <div className={styles.LoaderContainer}>
-      <LoaderSpinner type="ThreeDots" color="#00BFFF" height={80} width={80} />
+      <LoaderSpinner type="ThreeDots" color="#3f51b5" height={80} width={80} />
     </div>
   );
 };
